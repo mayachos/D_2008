26ddb780273408d118a06f81cbf2e730c38bda49
mona-apk <cordion_nap@yahoo.co.jp>
2020-11-01 00:38:19 +0900

    feat: 起動時に, Dynamic Linkで起動されるのかを確認

--- lib/presentation/reasobi_app.dart
+++ lib/presentation/reasobi_app.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:d_2008/firebase/dynamic_link/dynamic_link_service.dart';
 import 'package:flutter/material.dart';
 
 class ReasobiApp extends StatefulWidget {
@@ -5,7 +8,37 @@ class ReasobiApp extends StatefulWidget {
   _ReasobiAppState createState() => _ReasobiAppState();
 }
 
-class _ReasobiAppState extends State<ReasobiApp> {
+class _ReasobiAppState extends State<ReasobiApp> with WidgetsBindingObserver {
+  final DynamicLinkService _dynamicLinkService = DynamicLinkService();
+  Timer _timerLink;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _timerLink = new Timer(
+        const Duration(milliseconds: 1000),
+        () {
+          _dynamicLinkService.retrieveDynamicLink(context);
+        },
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    if (_timerLink != null) {
+      _timerLink.cancel();
+    }
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
