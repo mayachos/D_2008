2c3b58eb050a961d600572ef2b04b874029c8df0
mona-apk <cordion_nap@yahoo.co.jp>
2020-11-01 00:34:15 +0900

    feat: TopWidgetの作成

---
+++ lib/presentation/reasobi_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ReasobiApp extends StatefulWidget {
+  @override
+  _ReasobiAppState createState() => _ReasobiAppState();
+}
+
+class _ReasobiAppState extends State<ReasobiApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
