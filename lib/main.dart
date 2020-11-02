91d739c1390c3b79007be650eeea0be52caddc94
mona-apk <cordion_nap@yahoo.co.jp>
2020-11-02 21:03:10 +0900

    feat: mainからReasobiAppを呼び出す

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,10 @@
-import 'package:d_2008/firebase/dynamic_link/dynamic_link_service.dart';
+import 'package:d_2008/presentation/reasobi_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:share/share.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  final DynamicLinkService _dynamicLinkService = DynamicLinkService();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          height: 50,
-          width: 100,
-          child: FutureBuilder<Uri>(
-            future: _dynamicLinkService.createDynamicLink(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                Uri uri = snapshot.data;
-                return FlatButton(
-                  color: Colors.amber,
-                  onPressed: () => Share.share(uri.toString()),
-                  child: Text('Share'),
-                );
-              } else {
-                return Container();
-              }
-            },
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(ReasobiApp());
 }
