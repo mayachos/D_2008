2022622d71c5465991629ff995002d363897efe2
mona-apk <cordion_nap@yahoo.co.jp>
2020-11-05 07:43:55 +0900

    feat: FireStoreの追加

--- lib/di/get_it.dart
+++ lib/di/get_it.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -6,5 +7,6 @@ final getItInstance = GetIt.I;
 
 Future init() async {
   getItInstance.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+  getItInstance.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
   getItInstance.registerSingletonAsync<SharedPreferences>(() async => await SharedPreferences.getInstance());
 }
