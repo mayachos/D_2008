d74d85dd2f87c176733072414ec46938cf20e61d
mona-apk <cordion_nap@yahoo.co.jp>
2020-11-05 03:08:44 +0900

    feat: Twitter API周りを担うクラスの作成

---
+++ lib/model/request/twitter_request.dart
@@ -0,0 +1,36 @@
+import 'package:d_2008/di/get_it.dart';
+import 'package:dart_twitter_api/api/twitter_client.dart';
+import 'package:dart_twitter_api/twitter_api.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../constants.dart';
+import '../../env_constants.dart';
+
+class TwitterRequest {
+  SharedPreferences prefs;
+  TwitterApi twitterApi;
+
+  TwitterRequest() {
+    this.prefs = getItInstance.get<SharedPreferences>();
+    this.twitterApi = TwitterApi(
+      client: TwitterClient(
+        consumerKey: twitterConsumerKey,
+        consumerSecret: twitterSecretConsumerKey,
+        token: prefs.getString(twitterAccessToken),
+        secret: prefs.getString(twitterSecret),
+      ),
+    );
+  }
+
+  Future<void> postTweet() async {
+    try {
+      String text = """
+      ネコ\n猫\n🐈
+      """;
+      twitterApi.tweetService.update(status: text);
+    } catch (error) {
+      debugPrint(error.toString());
+    }
+  }
+}
