1f317f08cfcb89db90aef9911e6c8476cda011f0
mona-apk <cordion_nap@yahoo.co.jp>
2020-11-05 01:47:36 +0900

    feat: InviteScreenの作成

---
+++ lib/presentation/screen/invite_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class InviteScreen extends StatefulWidget {
+  @override
+  _InviteScreenState createState() => _InviteScreenState();
+}
+
+class _InviteScreenState extends State<InviteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("REASOBI"),
+      ),
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
