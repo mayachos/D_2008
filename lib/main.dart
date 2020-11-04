288f0eab035cdb9a682f0acc18e1b802299cee8d
mona-apk <cordion_nap@yahoo.co.jp>
2020-11-04 19:20:16 +0900

    feat: get_itの初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:d_2008/presentation/reasobi_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pedantic/pedantic.dart';
+
+import 'di/get_it.dart' as getIt;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  unawaited(getIt.init());
 
   runApp(ReasobiApp());
 }
