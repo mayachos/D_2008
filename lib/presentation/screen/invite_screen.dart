02fc95d1f09fdf3b7906f587b2c285ad24b699c8
mona-apk <cordion_nap@yahoo.co.jp>
2020-11-05 02:15:11 +0900

    style: TextFieldの追加

--- lib/presentation/screen/invite_screen.dart
+++ lib/presentation/screen/invite_screen.dart
@@ -6,6 +6,16 @@ class InviteScreen extends StatefulWidget {
 }
 
 class _InviteScreenState extends State<InviteScreen> {
+  final titleController = TextEditingController();
+  final detailController = TextEditingController();
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    detailController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +23,32 @@ class _InviteScreenState extends State<InviteScreen> {
         title: Text("REASOBI"),
       ),
       body: SafeArea(
-        child: Container(),
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 40),
+                TextField(
+                  style: TextStyle(fontSize: 24),
+                  decoration: InputDecoration(hintText: 'タイトル'),
+                  controller: titleController,
+                  minLines: 1,
+                  maxLines: 2,
+                  maxLength: 20,
+                  maxLengthEnforced: true,
+                ),
+                SizedBox(height: 16),
+                TextField(
+                  decoration: InputDecoration(hintText: '詳細'),
+                  controller: detailController,
+                  maxLength: 80,
+                  maxLengthEnforced: true,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
       floatingActionButton: true
           ? FloatingActionButton(
