dcf773feed4f5aedf62869b57f586e1e2dc97ede
mona-apk <cordion_nap@yahoo.co.jp>
2020-11-05 00:08:56 +0900

    feat: SharedPreferencesの記述

--- lib/di/get_it.dart
+++ lib/di/get_it.dart
@@ -1,5 +1,8 @@
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final getItInstance = GetIt.I;
 
-Future init() async {}
+Future init() async {
+  getItInstance.registerSingletonAsync<SharedPreferences>(() async => await SharedPreferences.getInstance());
+}
