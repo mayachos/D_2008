6bce631ed2e0d0b7e0abf4b24be66f68d24eb582
mona-apk <cordion_nap@yahoo.co.jp>
2020-11-05 01:41:41 +0900

    feat: HomeScreenの修正

--- lib/presentation/screen/home_screen.dart
+++ lib/presentation/screen/home_screen.dart
@@ -1,33 +1,24 @@
-import 'package:d_2008/firebase/dynamic_link/dynamic_link_service.dart';
 import 'package:flutter/material.dart';
-import 'package:share/share.dart';
 
 class HomeScreen extends StatelessWidget {
-  final DynamicLinkService _dynamicLinkService = DynamicLinkService();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          height: 50,
-          width: 100,
-          child: FutureBuilder<Uri>(
-            future: _dynamicLinkService.createInviteDynamicLink(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                Uri uri = snapshot.data;
-                return FlatButton(
-                  color: Colors.amber,
-                  onPressed: () => Share.share(uri.toString()),
-                  child: Text('Share'),
-                );
-              } else {
-                return Container();
-              }
-            },
-          ),
+      appBar: AppBar(
+        title: Text("REASOBI"),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          // List View
+          child: ListView(),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // TODO: 実装
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Color.fromRGBO(0, 150, 136, 1.0),
       ),
     );
   }
