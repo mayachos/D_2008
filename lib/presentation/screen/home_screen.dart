4c843f9acdbb9d26d85964e5aac4c3b34f5d8a6c
mona-apk <cordion_nap@yahoo.co.jp>
2020-11-05 23:24:03 +0900

    fix: 自分が参加しているInviteを取得する実装の修正

--- lib/presentation/screen/home_screen.dart
+++ lib/presentation/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:d_2008/di/get_it.dart';
+import 'package:d_2008/domain/entity/invite_entity.dart';
 import 'package:d_2008/presentation/screen/invite_screen.dart';
 import 'package:d_2008/presentation/transition/fade_route.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -34,29 +35,20 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
+  // 参加者の
   Future<void> fetchInvites() async {
     final User currentUser = getItInstance.get<User>();
     final UserInfo userInfo = currentUser.providerData.first;
+    List<InviteEntity> invites = [];
     CollectionReference invitesRef = FirebaseFirestore.instance.collection('invites');
-    // Note: 参加している募集を取得
     invitesRef.where("participantsUid", arrayContainsAny: [userInfo.uid]).get().then(
           (QuerySnapshot querySnapshot) {
             querySnapshot.docs.forEach(
               (doc) {
-                // Note: 参加者一覧を取得するサンプル実装
-                List<dynamic> participants = doc.data()["participantsRef"];
-                participants.forEach(
-                  (participant) {
-                    debugPrint(participant.toString());
-                    participant.get().then(
-                      (userQuerySnapshot) {
-                        debugPrint(userQuerySnapshot.data().toString());
-                      },
-                    );
-                  },
-                );
+                invites.add(InviteEntity.fromData(doc.data(), doc.id));
               },
             );
+            return invites;
           },
         );
   }
