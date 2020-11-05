ee2fa522d567a0e5e39ee0ed0145355466f049cd
mona-apk <cordion_nap@yahoo.co.jp>
2020-11-06 01:06:01 +0900

    feat: DynamicLinkからIDを受け取る実装

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,6 @@
 // Twitterログイン
 final String twitterAccessToken = "twitterAccessToken";
 final String twitterSecret = "twitterSecret";
+
+// 保存用
+final String inviteKey = "inviteKey";
--- lib/firebase/dynamic_link/dynamic_link_service.dart
+++ lib/firebase/dynamic_link/dynamic_link_service.dart
@@ -1,19 +1,27 @@
+import 'package:d_2008/di/get_it.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../constants.dart';
 
 class DynamicLinkService {
   Future<void> retrieveDynamicLink(BuildContext context) async {
     try {
       final PendingDynamicLinkData data = await FirebaseDynamicLinks.instance.getInitialLink();
       final Uri deepLink = data?.link;
+      final SharedPreferences prefs = getItInstance.get<SharedPreferences>();
 
-      // TODO: implementation
       if (deepLink != null) {
         debugPrint("Open Deep Link: $deepLink");
+        String invitedId = deepLink.queryParameters["id"];
+        prefs.setString(inviteKey, invitedId);
       }
 
       FirebaseDynamicLinks.instance.onLink(onSuccess: (PendingDynamicLinkData dynamicLink) async {
         debugPrint("Open Dynamic Link: ${dynamicLink.link}");
+        final String invitedId = Uri.parse(dynamicLink.link.toString()).queryParameters["id"];
+        prefs.setString(inviteKey, invitedId);
       });
     } catch (e) {
       debugPrint(e.toString());
