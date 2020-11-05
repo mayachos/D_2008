bdb8bb7d5b0a76bfce8f74b619311f9b00e55a5a
mona-apk <cordion_nap@yahoo.co.jp>
2020-11-05 09:52:17 +0900

    feat: 参加しているInviteの取得

--- lib/presentation/screen/home_screen.dart
+++ lib/presentation/screen/home_screen.dart
@@ -1,10 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:d_2008/di/get_it.dart';
 import 'package:d_2008/presentation/screen/invite_screen.dart';
 import 'package:d_2008/presentation/transition/fade_route.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    fetchInvites();
     return Scaffold(
       appBar: AppBar(
         title: Text("REASOBI"),
@@ -28,5 +32,27 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Color.fromRGBO(0, 150, 136, 1.0),
       ),
     );
+  }
+
+  Future<void> fetchInvites() async {
+    final User currentUser = getItInstance.get<User>();
+    final UserInfo userInfo = currentUser.providerData.first;
+    CollectionReference invitesRef = FirebaseFirestore.instance.collection('invites');
+    invitesRef
+        .where("participants", arrayContainsAny: [
+          {
+            "uid": userInfo.uid,
+            "displayName": userInfo.displayName,
+            "photoURL": userInfo.photoURL,
+          }
+        ])
+        .get()
+        .then(
+          (QuerySnapshot querySnapshot) {
+            querySnapshot.docs.forEach((doc) {
+              debugPrint(doc.data().toString());
+            });
+          },
+        );
   }
 }
