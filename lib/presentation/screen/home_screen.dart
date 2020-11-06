c4dc25e08a4f9342f11f547025705cbf326ec8f2
h.mikami <h.mikami@jxpress.net>
2020-11-07 07:17:40 +0900

    feat: 自分が参加している予定の数だけタイムラインに表示

--- lib/presentation/screen/home_screen.dart
+++ lib/presentation/screen/home_screen.dart
@@ -1,9 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:d_2008/di/get_it.dart';
-import 'package:d_2008/domain/entity/invite_entity.dart';
 import 'package:d_2008/presentation/screen/invite_screen.dart';
 import 'package:d_2008/presentation/transition/fade_route.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:d_2008/presentation/widget/invite_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -15,7 +12,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    fetchInvites();
     return Scaffold(
       appBar: AppBar(
         title: Text("REASOBI"),
@@ -24,12 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: SafeArea(
         child: WillPopScope(
-          child: Container(
-            child: SingleChildScrollView(
-              // TODO: Change List View
-              child: Container(),
-            ),
-          ),
+          child: InviteListView(),
           onWillPop: () {
             return SystemChannels.platform.invokeMethod('SystemNavigator.pop');
           },
@@ -46,22 +37,5 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Color.fromRGBO(0, 150, 136, 1.0),
       ),
     );
-  }
-
-  Future<void> fetchInvites() async {
-    final User currentUser = getItInstance.get<User>();
-    final UserInfo userInfo = currentUser.providerData.first;
-    List<InviteEntity> invites = [];
-    CollectionReference invitesRef = FirebaseFirestore.instance.collection('invites');
-    invitesRef.where("participantsUid", arrayContainsAny: [userInfo.uid]).get().then(
-          (QuerySnapshot querySnapshot) {
-            querySnapshot.docs.forEach(
-              (doc) {
-                invites.add(InviteEntity.fromData(doc.data(), doc.id));
-              },
-            );
-            return invites;
-          },
-        );
   }
 }
