4fdbf6c01152642090c1b5ee5c02eb703af3df95
Hideki Nihommatsu <tech.hidewagon2000@gmail.com>
2020-11-07 04:03:18 +0900

    SpaceBoxクラスもhome_screen.dartのものに一本化（正直このクラス必要なかったかも…）

--- lib/presentation/screen/author_invite_close_screen.dart
+++ lib/presentation/screen/author_invite_close_screen.dart
@@ -98,14 +98,6 @@ class _State extends State<AuthorInviteClose> {
   }
 }
 
-class SpaceBox extends SizedBox {
-  SpaceBox({double width = 8, double height = 8})
-      : super(width: width, height: height);
-
-  SpaceBox.width([double value = 8]) : super(width: value);
-  SpaceBox.height([double value = 8]) : super(height: value);
-}
-
 class ParticipantCard extends StatelessWidget {
   final String _pic;
   final String _username;
